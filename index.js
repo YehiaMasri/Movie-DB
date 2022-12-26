03955a09d13997acc1b28abd3890100998c3e7d0
YehiaMasri <yehiamasri83@gmail.com>
2022-12-26 18:25:11 +0200

    step 2

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const PORT=3000;
+
+const app = express();
+
+app.get('/' , (req, res )=> {
+    res.send('ok');
+});
+
+app.listen(PORT, () => console.log(`server in now listening on port ${PORT}`))
