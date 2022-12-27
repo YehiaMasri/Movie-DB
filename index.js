73e67353f1e0ef8720930da2796fb334e1fa4a9a
YehiaMasri <yehiamasri83@gmail.com>
2022-12-27 15:22:11 +0200

    step 7

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const PORT=3000;
+const PORT = 3000;
 
 const app = express();
 
@@ -11,62 +11,73 @@ const movies = [
     { title: 'الإرهاب والكباب', year: 1992, rating: 6.2 }
 ]
 
-app.get('/' , (req, res )=> {
+app.get('/', (req, res) => {
     res.send('ok');
 });
 
-app.get('/test', (req, res)=>{
-    res.status(200).send({status:200, message:'ok'});
+app.get('/test', (req, res) => {
+    res.status(200).send({ status: 200, message: 'ok' });
 });
 
-app.get('/time', (req, res)=>{
-    let time= new Date();
-    res.status(200).send({status:200, message:`${time.getHours()}:${time.getMinutes()}}`})
+app.get('/time', (req, res) => {
+    let time = new Date();
+    res.status(200).send({ status: 200, message: `${time.getHours()}:${time.getMinutes()}}` })
 });
 
-app.get('/hello/:userID', (req, res)=>{
-    res.status(200).send({status:200, message:`Hello ${req.params.userID}`})
+app.get('/hello/:userID', (req, res) => {
+    res.status(200).send({ status: 200, message: `Hello ${req.params.userID}` })
 });
 
-app.get('/search', (req, res)=>{
-    if(req.query.s){
-        res.status(200).send({status:200, message:"ok", data:req.query.s})
-    }else{
-        res.status(500).send({status:500, error:true, message:"you have to provide a search"})
-        }
+app.get('/search', (req, res) => {
+    if (req.query.s) {
+        res.status(200).send({ status: 200, message: "ok", data: req.query.s })
+    } else {
+        res.status(500).send({ status: 500, error: true, message: "you have to provide a search" })
+    }
 });
 
-app.get('/movies/create', (req, res) =>{
+app.get('/movies/create', (req, res) => {
     res.send("mc");
 });
 
-app.get ('/movies/read', (req, res)=>{
-    res.status(200).send({status: 200, data:movies })
-}); 
+app.get('/movies/read', (req, res) => {
+    res.status(200).send({ status: 200, data: movies })
+});
 
-app.get('/movies/update', (req, res)=>{
+app.get('/movies/update', (req, res) => {
     res.send('mu')
 })
 
-app.get('/movies/delete', (req, res)=>{
+app.get('/movies/delete', (req, res) => {
     res.send('md');
 })
 
- app.get('/movies/read/:text', (req, res)=>{
-     if(req.params.text==="by-date"){
-         res.status(200).send({status:200, data:movies.sort((a, b)=> a.year - b.year)})
-     }else if(req.params.text==="by-rating"){
-         res.status(200).send({status:200, data:movies.sort((a, b)=>b.rating - a.rating)})
-     }else if (req.params.text==="by-title"){
-         res.status(200).send({status:200, data:movies.sort((a, b)=>{if(a.title>b.title){
-             return 1;
-         }else if (b.title>a.title){
-             return -1;
-         }else{
-             return 0;
-         } 
-     })})
-      }
- });
+app.get('/movies/read/:text', (req, res) => {
+    if (req.params.text === "by-date") {
+        res.status(200).send({ status: 200, data: movies.sort((a, b) => a.year - b.year) })
+    } else if (req.params.text === "by-rating") {
+        res.status(200).send({ status: 200, data: movies.sort((a, b) => b.rating - a.rating) })
+    } else if (req.params.text === "by-title") {
+        res.status(200).send({
+            status: 200, data: movies.sort((a, b) => {
+                if (a.title > b.title) {
+                    return 1;
+                } else if (b.title > a.title) {
+                    return -1;
+                } else {
+                    return 0;
+                }
+            })
+        })
+    }
+});
+
+app.get('/movies/read/id/:ID', (req, res) => {
+    if (req.params.ID >= 0 && req.params.ID < movies.length) {
+        res.status(200).send({ status: 200, data: movies[req.params.ID] })
+    } else {
+        res.status(404).send({ status: 404, error: true, message: `the movie ${req.params.ID} does not exist` })
+    }
+});
 
 app.listen(PORT, () => console.log(`server in now listening on port ${PORT}`))
